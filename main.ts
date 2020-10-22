b0fcca9c1283f3b76c6b78d45c50d6f9ad983cbb
Philipp Moritzer <moritzer@ascora.de>
2020-10-22 09:22:05 +0200

    update

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, screen } from "electron";
+import { app, BrowserWindow, ipcMain, ipcRenderer, screen } from "electron";
 import * as path from "path";
 import * as url from "url";
 import { autoUpdater } from "electron-updater";
@@ -25,7 +25,19 @@ function createWindow(): BrowserWindow {
     },
   });
 
-  autoUpdater.checkForUpdatesAndNotify();
+  win.once("ready-to-show", () => {
+    autoUpdater.checkForUpdatesAndNotify();
+  });
+
+  autoUpdater.on("update-available", () => {
+    win.webContents.send("update_available");
+  });
+  autoUpdater.on("update-downloaded", () => {
+    win.webContents.send("update_downloaded");
+  });
+  ipcMain.on("restart_app", () => {
+    autoUpdater.quitAndInstall();
+  });
 
   if (serve) {
     win.webContents.openDevTools();
@@ -78,6 +90,22 @@ try {
       createWindow();
     }
   });
+
+  // const notification = document.getElementById("notification");
+  // const message = document.getElementById("message");
+  // const restartButton = document.getElementById("restart-button");
+  // ipcRenderer.on("update_available", () => {
+  //   ipcRenderer.removeAllListeners("update_available");
+  //   message.innerText = "A new update is available. Downloading now...";
+  //   notification.classList.remove("hidden");
+  // });
+  // ipcRenderer.on("update_downloaded", () => {
+  //   ipcRenderer.removeAllListeners("update_downloaded");
+  //   message.innerText =
+  //     "Update Downloaded. It will be installed on restart. Restart now?";
+  //   restartButton.classList.remove("hidden");
+  //   notification.classList.remove("hidden");
+  // });
 } catch (e) {
   // Catch Error
   // throw e;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,32 @@
-import { Component } from '@angular/core';
-import { ElectronService } from './core/services';
-import { TranslateService } from '@ngx-translate/core';
-import { AppConfig } from '../environments/environment';
+import { Component } from "@angular/core";
+import { ElectronService } from "./core/services";
+import { TranslateService } from "@ngx-translate/core";
+import { AppConfig } from "../environments/environment";
+import { ipcRenderer } from "electron";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
   constructor(
     private electronService: ElectronService,
     private translate: TranslateService
   ) {
-    this.translate.setDefaultLang('en');
-    console.log('AppConfig', AppConfig);
+    this.translate.setDefaultLang("en");
+    console.log("AppConfig", AppConfig);
 
     if (electronService.isElectron) {
       console.log(process.env);
-      console.log('Run in electron');
-      console.log('Electron ipcRenderer', this.electronService.ipcRenderer);
-      console.log('NodeJS childProcess', this.electronService.childProcess);
+      console.log("Run in electron");
+      console.log("Electron ipcRenderer", this.electronService.ipcRenderer);
+      console.log("NodeJS childProcess", this.electronService.childProcess);
     } else {
-      console.log('Run in browser');
+      console.log("Run in browser");
     }
+  }
+  restartApp() {
+    ipcRenderer.send("restart_app");
   }
 }
